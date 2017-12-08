31d3869d11c57debb04c89424531563c53c1b8af
Airdin93 <emil.johansen93@gmail.com>
2017-12-08 13:06:05 +0100

    Began on the options site

--- script/script.js
+++ script/script.js
@@ -13,7 +13,7 @@ function indexMap() {
 function doctorMap() {
   var location1 = {lat: 55.4880017, lng: 8.44878510000001};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
+    zoom: 16,
     center: location1
   });
   var marker = new google.maps.Marker({
@@ -25,7 +25,7 @@ function doctorMap() {
 function frirumshusetMap() {
   var location1 = {lat: 55.4890114, lng: 8.44416960000001};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
+    zoom: 16,
     center: location1
   });
   var marker = new google.maps.Marker({
@@ -37,7 +37,7 @@ function frirumshusetMap() {
 function indkobMap() {
   var location1 = {lat: 55.4871874, lng: 8.444064300000036};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
+    zoom: 16,
     center: location1
   });
   var marker = new google.maps.Marker({
@@ -49,7 +49,19 @@ function indkobMap() {
 function klubhusetMap() {
   var location1 = {lat: 55.487211, lng: 8.445298999999977};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
+    zoom: 16,
+    center: location1
+  });
+  var marker = new google.maps.Marker({
+    position: location1,
+    map: map
+  });
+}
+
+function hvorErVi() {
+  var location1 = {lat: 55.487784, lng: 8.446825999999987};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
     center: location1
   });
   var marker = new google.maps.Marker({
