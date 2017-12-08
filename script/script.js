3f8658e7944c0df841178978b620914ac267f700
Airdin93 <emil.johansen93@gmail.com>
2017-12-08 11:37:27 +0100

    Fixed the map locations

--- script/script.js
+++ script/script.js
@@ -1,17 +1,53 @@
-function initMap() {
-    var location1 = {lat: 55.4880017, lng: 8.44878510000001};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 15,
-      center: location1
-    });
-    var marker = new google.maps.Marker({
-      position: location1,
-      map: map
-    });
-}
-
 function indexMap() {
   var location1 = {lat: 55.487784, lng: 8.446825999999987};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: location1
+  });
+  var marker = new google.maps.Marker({
+    position: location1,
+    map: map
+  });
+}
+
+function doctorMap() {
+  var location1 = {lat: 55.4880017, lng: 8.44878510000001};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: location1
+  });
+  var marker = new google.maps.Marker({
+    position: location1,
+    map: map
+  });
+}
+
+function frirumshusetMap() {
+  var location1 = {lat: 55.4890114, lng: 8.44416960000001};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: location1
+  });
+  var marker = new google.maps.Marker({
+    position: location1,
+    map: map
+  });
+}
+
+function indkobMap() {
+  var location1 = {lat: 55.4871874, lng: 8.444064300000036};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: location1
+  });
+  var marker = new google.maps.Marker({
+    position: location1,
+    map: map
+  });
+}
+
+function klubhusetMap() {
+  var location1 = {lat: 55.487211, lng: 8.445298999999977};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: location1
