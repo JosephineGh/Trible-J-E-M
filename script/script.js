305e07cbf30ffc30dd7565f29040a96c6f90e881
Airdin93 <emil.johansen93@gmail.com>
2017-12-08 11:25:59 +0100

    javascript Adjusted

--- script/script.js
+++ script/script.js
@@ -9,3 +9,15 @@ function initMap() {
       map: map
     });
 }
+
+function indexMap() {
+  var location1 = {lat: 55.487784, lng: 8.446825999999987};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: location1
+  });
+  var marker = new google.maps.Marker({
+    position: location1,
+    map: map
+  });
+}
